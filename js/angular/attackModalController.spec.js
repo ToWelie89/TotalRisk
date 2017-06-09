9c6c2197e746b3ab7220c6b44e030e256228618d
ToWelie89 <sonesson8909@hotmail.com>
2017-06-09 14:43:31 +0200

    Karma test

--- js/angular/attackModalController.spec.js
+++ js/angular/attackModalController.spec.js
@@ -1,3 +1,4 @@
+/*
 describe('attackModalController', function() {
 
     let attackModalController;
@@ -35,5 +36,30 @@ describe('attackModalController', function() {
         expect(attackModalController.fightIsOver).toEqual(false);
         expect(attackModalController.showMoveTroops).toEqual(false);
         expect(attackModalController.moveNumberOfTroops).toEqual(1);
+    });
+});
+*/
+
+describe('attackModalController', function() {
+
+    var $rootScope, $scope, $controller, $log, attackModalController;
+
+    // beforeEach(module('risk'));
+    angular.mock.module('risk');
+
+    beforeEach(inject(function(_$rootScope_, _$controller_){
+        $rootScope = _$rootScope_;
+        $scope = $rootScope.$new();
+        $controller = _$controller_;
+
+        angular.mock.inject(function ($injector) {
+            $log = $injector.get('$log');
+        });
+
+        attackModalController = $controller('attackModalController', {'$rootScope' : $rootScope, '$scope': $scope, '$log': $log, 'soundService': {}});
+    }));
+
+    it('should exist', function() {
+        expect(attackModalController).toBeDefined();
     });
 });
