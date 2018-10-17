a64c39537b1382ac01dfd2cd0ee42d8f73a5c82f
ToWelie89 <sonesson8909@hotmail.com>
2018-10-17 16:07:05 +0200

    added unit tests

---
+++ js/battleHandler.test.js
@@ -0,0 +1,115 @@
+import BattleHandler from './battleHandler';
+
+describe('BattleHandler', () => {
+
+    let battleHandler;
+
+    beforeEach(() => {
+        battleHandler = new BattleHandler();
+    });
+
+    it('sortDescending sorts list of dice correctly - attacker wins both rolls', () => {
+        const attacker = {
+            numberOfTroops: 5
+        };
+        const defender = {
+            numberOfTroops: 4
+        };
+
+        const attackDice = [1, 5, 4];
+        const defendDice = [2, 1];
+
+        const handleAttackResponse = battleHandler.handleAttack(attacker, defender, attackDice, defendDice);
+
+        expect(handleAttackResponse.attackDice).toEqual([5, 4, 1]);
+        expect(handleAttackResponse.defendDice).toEqual([2, 1]);
+        expect(handleAttackResponse.attacker).toEqual({ numberOfTroops: 5 });
+        expect(handleAttackResponse.defender).toEqual({ numberOfTroops: 2 });
+        expect(handleAttackResponse.defenderCasualties).toEqual(2);
+        expect(handleAttackResponse.attackerCasualties).toEqual(0);
+    });
+
+    it('sortDescending sorts list of dice correctly - defender wins both rolls', () => {
+        const attacker = {
+            numberOfTroops: 10
+        };
+        const defender = {
+            numberOfTroops: 7
+        };
+
+        const attackDice = [2, 3, 1];
+        const defendDice = [5, 6];
+
+        const handleAttackResponse = battleHandler.handleAttack(attacker, defender, attackDice, defendDice);
+
+        expect(handleAttackResponse.attackDice).toEqual([3, 2, 1]);
+        expect(handleAttackResponse.defendDice).toEqual([6, 5]);
+        expect(handleAttackResponse.attacker).toEqual({ numberOfTroops: 8 });
+        expect(handleAttackResponse.defender).toEqual({ numberOfTroops: 7 });
+        expect(handleAttackResponse.defenderCasualties).toEqual(0);
+        expect(handleAttackResponse.attackerCasualties).toEqual(2);
+    });
+
+    it('sortDescending sorts list of dice correctly - both wins one each', () => {
+        const attacker = {
+            numberOfTroops: 18
+        };
+        const defender = {
+            numberOfTroops: 4
+        };
+
+        const attackDice = [5, 2, 2];
+        const defendDice = [4, 3];
+
+        const handleAttackResponse = battleHandler.handleAttack(attacker, defender, attackDice, defendDice);
+
+        expect(handleAttackResponse.attackDice).toEqual([5, 2, 2]);
+        expect(handleAttackResponse.defendDice).toEqual([4, 3]);
+        expect(handleAttackResponse.attacker).toEqual({ numberOfTroops: 17 });
+        expect(handleAttackResponse.defender).toEqual({ numberOfTroops: 3 });
+        expect(handleAttackResponse.defenderCasualties).toEqual(1);
+        expect(handleAttackResponse.attackerCasualties).toEqual(1);
+    });
+
+    it('number of dice should be generated correctly based on numberOfTroops for both attacker and defender', () => {
+        const attacker = {
+            numberOfTroops: 5
+        };
+        const defender = {
+            numberOfTroops: 5
+        };
+
+        const handleAttackResponse = battleHandler.handleAttack(attacker, defender);
+
+        expect(handleAttackResponse.attackDice.length).toEqual(3);
+        expect(handleAttackResponse.defendDice.length).toEqual(2);
+    });
+
+    it('number of dice should be generated correctly based on numberOfTroops for both attacker and defender', () => {
+        const attacker = {
+            numberOfTroops: 2
+        };
+        const defender = {
+            numberOfTroops: 2
+        };
+
+        const handleAttackResponse = battleHandler.handleAttack(attacker, defender);
+
+        expect(handleAttackResponse.attackDice.length).toEqual(2);
+        expect(handleAttackResponse.defendDice.length).toEqual(2);
+    });
+
+    it('number of dice should be generated correctly based on numberOfTroops for both attacker and defender', () => {
+        const attacker = {
+            numberOfTroops: 1
+        };
+        const defender = {
+            numberOfTroops: 1
+        };
+
+        const handleAttackResponse = battleHandler.handleAttack(attacker, defender);
+
+        expect(handleAttackResponse.attackDice.length).toEqual(1);
+        expect(handleAttackResponse.defendDice.length).toEqual(1);
+    });
+});
---
+++ js/deploymentHandler.test.js
@@ -0,0 +1,117 @@
+import DeploymentHandler from './deploymentHandler';
+import Player from './player/player';
+import WorldMap from './map/worldMap';
+import { worldMap } from './map/worldMapConfiguration';
+import { CONSTANTS } from './gameConstants';
+
+describe('DeploymentHandler', () => {
+
+    let deploymentHandler;
+
+    beforeEach(() => {
+        deploymentHandler = new DeploymentHandler();
+    });
+
+    it('calculateReinforcements - all territories controlled by player', () => {
+        const players = new Map();
+        ['Caesar', 'Napoleon'].forEach(playerName => {
+            players.set(playerName, new Player(playerName, 'color', 'avatar'));
+        });
+        const map = new WorldMap();
+        const turn = {
+            player: {
+                name: 'Caesar'
+            }
+        };
+
+        map.getAllTerritoriesAsList().forEach(territory => {
+            territory.owner = 'Caesar'
+        });
+
+        let points = Math.floor(map.getAllTerritoriesAsList().length / 3);
+        map.regions.forEach(region => {
+            points += region.bonusTroops
+        });
+
+        const response = deploymentHandler.calculateReinforcements(players, map, turn);
+        expect(response).toEqual(points);
+    });
+
+    it('calculateReinforcements - Africa and Europe controlled by player', () => {
+        const players = new Map();
+        ['Caesar', 'Napoleon'].forEach(playerName => {
+            players.set(playerName, new Player(playerName, 'color', 'avatar'));
+        });
+        const map = new WorldMap();
+        const turn = {
+            player: {
+                name: 'Caesar'
+            }
+        };
+
+        map.regions.forEach(region => {
+            if (region.name === 'Africa' || region.name === 'Europe') {
+                region.territories.forEach(territory => {
+                    territory.owner = 'Caesar'
+                })
+            }
+        });
+
+        const europe = worldMap.regions.find(region => region.name === 'Europe');
+        const africa = worldMap.regions.find(region => region.name === 'Africa');
+
+        let points = Math.floor((europe.territories.length + africa.territories.length) / 3);
+        points += africa.bonusTroops;
+        points += europe.bonusTroops;
+
+        const response = deploymentHandler.calculateReinforcements(players, map, turn);
+        expect(response).toEqual(points);
+    });
+
+    it('calculateReinforcements - Asia controlled by player', () => {
+        const players = new Map();
+        ['Caesar', 'Napoleon'].forEach(playerName => {
+            players.set(playerName, new Player(playerName, 'color', 'avatar'));
+        });
+        const map = new WorldMap();
+        const turn = {
+            player: {
+                name: 'Caesar'
+            }
+        };
+
+        map.regions.forEach(region => {
+            if (region.name === 'Asia') {
+                region.territories.forEach(territory => {
+                    territory.owner = 'Caesar'
+                })
+            }
+        });
+
+        const asia = worldMap.regions.find(region => region.name === 'Asia');
+
+        let points = Math.floor(asia.territories.length / 3);
+        points += asia.bonusTroops;
+
+        const response = deploymentHandler.calculateReinforcements(players, map, turn);
+        expect(response).toEqual(points);
+    });
+
+    it('calculateReinforcements - Only one territory controlled by player', () => {
+        const players = new Map();
+        ['Caesar', 'Napoleon'].forEach(playerName => {
+            players.set(playerName, new Player(playerName, 'color', 'avatar'));
+        });
+        const map = new WorldMap();
+        const turn = {
+            player: {
+                name: 'Caesar'
+            }
+        };
+
+        map.getAllTerritoriesAsList()[0].owner = 'Caesar';
+
+        const response = deploymentHandler.calculateReinforcements(players, map, turn);
+        expect(response).toEqual(CONSTANTS.MIN_REINFORCEMENTS);
+    });
+});
